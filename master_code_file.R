b79b6343ec9910324ffb03ce46d445c1f688d4a3
generationai <65367927+generationai@users.noreply.github.com>
2021-04-29 16:29:00 -0400

    Update master_code_file.R

--- master_code_file.R
+++ master_code_file.R
@@ -28,15 +28,16 @@ counties_coord <- geojson_read("https://raw.githubusercontent.com/generationai/t
 leaflet(data = counties_coord) %>% #input our GeoJSON to the leaflet met
   addTiles() %>%
   setView(-98.5795, 39.828175, zoom = 4) %>% #set our view to the center of the United States
+  #Try these latitude and longitude coordinates: -77.007507, 38.900497 - what does it seem to be centered on?
   addPolygons() #Add shapes based on the GeoJSON file
 
 #End of code chunk #2
 #_____________________________________________________________________#
 #Beginning of code chunk #3
 
-leaflet(counties_coord) %>%
+leaflet(data = counties_coord) %>%
   addTiles() %>%
-  setView(-98.5795, 39.828175, zoom = 4) %>%
+  setView(-98.5795, 39.828175, zoom = 4) %>% #change the zoom level to see an ideal zoom value
   addPolygons(
     color = "#444444", #There are different color codes that can be found online - try changing this to "gold"
     weight = 0.3) #change this value to see how the thickness of the 
@@ -50,7 +51,7 @@ recent_covid_counties <- read.csv('https://raw.githubusercontent.com/generationa
 
 qpal <- colorQuantile("YlOrRd", recent_covid_counties$cases) #create a color palette
 
-leaflet(counties_coord) %>% 
+leaflet(data = counties_coord) %>% 
   addTiles() %>%
   setView(-98.5795, 39.828175, zoom = 4) %>%
   addPolygons(
@@ -64,15 +65,16 @@ leaflet(counties_coord) %>%
 #_____________________________________________________________________#
 #Beginning of code chunk #5
 
-ui <- fluidPage(
+ui <- fluidPage(        #UI Component
   h1("This is an app!") #HTML header text 
+  #, h2("App created by: ")
 )
 
-server <- function(input, output){ #no interactivity or visualizations because this is empty
-  
+server <- function(input, output){  #Server Component
+                                    #no interactivity or visualizations because this is empty
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)  #shinyApp() function component
 
 #End of code chunk #5
 #_____________________________________________________________________#
@@ -81,12 +83,12 @@ shinyApp(ui = ui, server = server)
 qpal <- colorQuantile("YlOrRd", recent_covid_counties$cases)
 
 ui <- fluidPage(
-  h2("This is an app!"),
+  h1("This is an app!"),
   leafletOutput("mymap") #tells the UI function to display the heatmap
 )
 server <- function(input, output){
   output$mymap <- renderLeaflet({ #we add this to render the leaflet heatmap
-    leaflet(counties_coord) %>% 
+    leaflet(data = counties_coord) %>% 
       addTiles() %>%
       setView(-98.5795, 39.828175, zoom = 4) %>%
       addPolygons(
@@ -114,13 +116,13 @@ new_cases <- read.csv("https://raw.githubusercontent.com/generationai/test-githu
   mutate(date = as.Date(date))
 
 #try changing the state name (make sure it's lowercase) to see a new graph
-ggplotly(new_cases[new_cases$state == 'alabama',] %>% 
-           ggplot( aes(x=date, y=new_cases)) +
-           geom_area(fill="#69b3a2", alpha=0.5) +
-           geom_line(color="#69b3a2") +
-           ylab("New Cases") +
-           xlab("Date") +
-           theme_ipsum())
+ggplotly(new_cases[new_cases$state == 'alabama',] %>% #set the state we're looking at to Alabama
+           ggplot( aes(x=date, y=new_cases)) + #set an x value of data and a y value of new cases
+           geom_area(fill="#69b3a2", alpha=0.5) + #color in the area under the graph
+           geom_line(color="#69b3a2") + #color in the line at the top
+           ylab("New Cases") + #create a y axis label
+           xlab("Date") + #create an x axis label
+           theme_ipsum()) #change the fonts of the text displayed
 
 #End of code chunk #8
 #_____________________________________________________________________#
@@ -130,7 +132,7 @@ new_cases <- read.csv("https://raw.githubusercontent.com/generationai/test-githu
   mutate(date = as.Date(date))
 
 ui <- fluidPage(
-  h2("This is my map"),
+  h1("This is my map"),
   selectInput("state_selected", #create an input selector widget 
               "pick a state",
               choice = unique(new_cases$state),
@@ -145,7 +147,7 @@ ui <- fluidPage(
 
 server <- function(input, output){
   output$mymap <- renderLeaflet({
-    leaflet(counties_coord) %>% 
+    leaflet(data = counties_coord) %>% 
       addTiles() %>%
       setView(-98.5795, 39.828175, zoom = 4) %>%
       addPolygons(
@@ -157,7 +159,7 @@ server <- function(input, output){
   })
   
   output$barplot <- renderPlotly({
-    ggplotly(new_cases[new_cases$state==input$state_selected,] %>% 
+    ggplotly(new_cases[new_cases$state==input$state_selected,] %>% #filter graph based on user's input
                ggplot( aes(x=date, y=new_cases)) +
                geom_area(fill="#69b3a2", alpha=0.5) +
                geom_line(color="#69b3a2") +
@@ -175,7 +177,7 @@ new_cases <- read.csv("https://raw.githubusercontent.com/generationai/test-githu
   mutate(date = as.Date(date))
 
 ui <- fluidPage(
-  h2("This is my map"),
+  h1("This is my map"),
   selectInput("state_selected", 
               "Pick a State:",
               choice = unique(new_cases$state),
@@ -186,7 +188,7 @@ ui <- fluidPage(
 
 server <- function(input, output){
   output$mymap <- renderLeaflet({
-    leaflet(counties_coord) %>% 
+    leaflet(data = counties_coord) %>% 
       addTiles() %>%
       setView(-98.5795, 39.828175, zoom = 4) %>%
       addPolygons(
@@ -212,7 +214,3 @@ shinyApp(ui=ui, server = server)
 
 #End of code chunk #11
 #_____________________________________________________________________#
-
-
-
-
